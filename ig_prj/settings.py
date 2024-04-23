7c620e4e35edaadd1905ae385f24c03730664099
Amrit Sinha <56241123+SinhaAmrit@users.noreply.github.com>
2024-04-23 12:13:59 +0530

    Update settings.py

--- ig_prj/settings.py
+++ ig_prj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'k1l8gsqp&&glb@%qafi26xb-y4%xjf60g)2!6bspqc+@xjl7me'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["pixagram-prj.onrender.com", "127.0.0.1"]
 
